0d70586504577fae3c8f970d598ab539dc77618a
John Palaganas <112512965+Johnpepsi@users.noreply.github.com>
2023-04-24 14:11:02 -0600

    Add files via upload

---
+++ ContentView.swift
@@ -0,0 +1,88 @@
+import SwiftUI
+
+struct ContentView: View {
+    
+    @State private var playerCard = "card6"
+    @State private var cpuCard = "card7"
+    @State private var playerScore = 0
+    @State private var cpuScore = 0
+    
+    var body: some View {
+        
+        ZStack {
+            
+            Image("background-cloth")
+                .ignoresSafeArea()
+            
+            VStack {
+                Spacer()
+                Image("logo")
+                Spacer()
+                HStack {
+                    Spacer()
+                    Image(playerCard)
+                    Spacer()
+                    Image(cpuCard)
+                    Spacer()
+                }
+                Spacer()
+                
+                Button(action: {
+                    
+                    // Generate a random number between 2 and 14
+                    let playerRand = Int.random(in: 2...14)
+                    let cpuRand = Int.random(in: 2...14)
+                    
+                    // Update the cards
+                    playerCard = "card" + String(playerRand)
+                    cpuCard = "card" + String(cpuRand)
+                    
+                    // Update the score
+                    if playerRand > cpuRand {
+                        playerScore += 1
+                    }
+                    else if cpuRand > playerRand {
+
+                        cpuScore += 1
+                    }
+                }, label: {
+                    Image("button")
+                })
+                
+                Spacer()
+                HStack {
+                    Spacer()
+                    VStack {
+                        Text("Player")
+                            .font(.headline)
+                            .foregroundColor(Color.white)
+                            .padding(.bottom, 10.0)
+                        Text(String(playerScore))
+                            .font(.largeTitle)
+                            .foregroundColor(Color.white)
+                    }
+                    Spacer()
+                    VStack {
+                        Text("CPU")
+                            .font(.headline)
+                            .fontWeight(.bold)
+                            .foregroundColor(Color.white)
+                            .padding(.bottom, 10.0)
+                        Text(String(cpuScore))
+                            .font(.largeTitle)
+                            .foregroundColor(Color.white)
+                            
+                    }
+                    Spacer()
+                }
+                Spacer()
+            }
+        }
+    }
+    
+    struct ContentView_Previews: PreviewProvider {
+        static var previews: some View {
+            ContentView()
+        }
+    }
+}
