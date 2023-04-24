420761bde94055c445ae15011031fb1763351478
John Palaganas <112512965+Johnpepsi@users.noreply.github.com>
2023-04-24 14:11:27 -0600

    Add files via upload

---
+++ war_gameApp.swift
@@ -0,0 +1,17 @@
+//
+//  war_gameApp.swift
+//  war_game
+//
+//  Created by ojohnpepsi on 2023-04-22.
+//
+
+import SwiftUI
+
+@main
+struct war_gameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
